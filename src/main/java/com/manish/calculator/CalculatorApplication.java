ebea2f166cb3c575b4c4af5ee1aba48083a2793a
manish <gautam.manish67@gmail.com>
2018-06-01 18:02:06 +0530

    static code analysis

--- src/main/java/com/manish/calculator/CalculatorApplication.java
+++ src/main/java/com/manish/calculator/CalculatorApplication.java
@@ -2,10 +2,11 @@ package com.manish.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+* Main Spring Application.
+*/
 @SpringBootApplication
 public class CalculatorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
